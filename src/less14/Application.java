355ec32e89b8003d1a41e1a87e8c118738de1f71
Hydronchik <kravchenko.rostik06@gmail.com>
2024-03-02 14:05:01 +0200

    sa

--- src/less14/Application.java
+++ src/less14/Application.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
+
     public static void menu(){
         int i = 0;
         Menu menus = new Menu();
@@ -17,61 +18,103 @@ public class Application {
             i ++;
         }
         System.out.println("====================["+ (i+1) +"]========================");
-        System.out.println("Подивитися список замовлень");
+        System.out.println("-------Подивитися список замовлень---------");
     }
     public static void main(String[] args) {
         Order order = new Order();
         menu();
         Scanner mainScanner = new Scanner(System.in);
         while (true){
-
+            System.out.println("Введіть номер: ");
             String mainCode = mainScanner.next();
             switch (mainCode){
                 case "1" ->{
                     System.out.println("Виберіть розмір: ");
-                    String sizeCaffeAmericano = mainScanner.next();
-                    if (sizeCaffeAmericano == "XL"){
-                    order.createOrder(new Caffe("Americano", 2, List.of(Sizes.XL)));
-                    } else if (sizeCaffeAmericano == "L") {
-                        order.createOrder(new Caffe("Americano", 2, List.of(Sizes.L)));
-                    }else if (sizeCaffeAmericano == "M") {
-                        order.createOrder(new Caffe("Americano", 2, List.of(Sizes.M)));
-                }else {
-                        System.out.println("Такого розміру немає:<");
+                    Scanner scanner = new Scanner(System.in);
+                    String sizeCaffeAmericano = scanner.next();
+                    switch (sizeCaffeAmericano){
+                        case "XL"->{
+                            order.createOrder(new Caffe("Americano", 2, List.of(Sizes.XL)));
+                            System.out.println("Кава Americano розміру XL додана до вашого кошика");
+                        }
+                        case "L"->{
+                            order.createOrder(new Caffe("Americano", 2, List.of(Sizes.L)));
+                            System.out.println("Кава Americano розміру L додана до вашого кошика");
+                        }
+                        case "M"->{
+                            order.createOrder(new Caffe("Americano", 2, List.of(Sizes.M)));
+                            System.out.println("Кава Americano розміру M додана до вашого кошика");
+                        }
 
                     }
                     break;
                 }
                 case "2" ->{
                     System.out.println("Виберіть розмір: ");
-                    String sizeCaffeCapuchino = mainScanner.next();
-                    if (sizeCaffeCapuchino == "S"){
-                        order.createOrder(new Caffe("Capuchino", 4, List.of(Sizes.S)));
-                    } else if (sizeCaffeCapuchino == "L") {
-                        order.createOrder(new Caffe("Capuchino", 4, List.of(Sizes.L)));
-
-                    }else {
-                        System.out.println("Такого розміру немає:<");
-
+                    Scanner scanner = new Scanner(System.in);
+                    String sizeCaffeCapuchino = scanner.next();
+                    switch (sizeCaffeCapuchino){
+                        case "S"->{
+                            order.createOrder(new Caffe("Capuchino", 4, List.of(Sizes.S)));
+                            System.out.println("Кава Capuchino розміру S додана до вашого кошика");
+                        }
+                        case "L"->{
+                            order.createOrder(new Caffe("Capuchino", 4, List.of(Sizes.L)));
+                            System.out.println("Кава Capuchino розміру L додана до вашого кошика");
+                        }
                     }
                     break;
                 }
                 case "3" ->{
                     System.out.println("Виберіть розмір: ");
-                    String sizeCaffeLatte = mainScanner.next();
-                    if (sizeCaffeLatte == "S"){
-                        order.createOrder(new Caffe("Capuchino", 2, List.of(Sizes.S)));
-                    } else if (sizeCaffeLatte == "L") {
-                        order.createOrder(new Caffe("Capuchino", 2, List.of(Sizes.L)));
-
-                    }else if (sizeCaffeLatte == "M") {
-                        order.createOrder(new Caffe("Capuchino", 2, List.of(Sizes.M)));
-
-                    }else {
-                        System.out.println("Такого розміру немає:<");
-
+                    Scanner scanner = new Scanner(System.in);
+                    String sizeCaffeLatte = scanner.next();
+                    switch (sizeCaffeLatte){
+                        case "S"->{
+                            order.createOrder(new Caffe("Latte", 2, List.of(Sizes.S)));
+                            System.out.println("Кава Latte розміру S додана до вашого кошика");
+                        }
+                        case "L"->{
+                            order.createOrder(new Caffe("Latte", 2, List.of(Sizes.L)));
+                            System.out.println("Кава Latte розміру L додана до вашого кошика");
+                        }
+                        case "M"->{
+                            order.createOrder(new Caffe("Latte", 2, List.of(Sizes.M)));
+                            System.out.println("Кава Latte розміру M додана до вашого кошика");
+                        }
                     }
                     break;
+                }
+                case "4" ->{
+                    int i = 0;
+                    int sum = order.getTotalPrice();
+                    System.out.println("===============Ваш список замовлень==================");
+                    for (Caffe c : order.getTotalList()){
+                        System.out.println(order.getTotalList().get(i).getName() + " розміром " + order.getTotalList().get(i).getSizes());
+                        sum = order.getTotalList().get(1).getPrice() + sum;
+
+                        i ++;
+                    }
+                    System.out.println("Ціна за все: " + sum);
+                    System.out.println("1 - Видалити кофе з замовлення");
+                    System.out.println("2 - Купити все");
+                    Scanner scanner = new Scanner(System.in);
+                    String finalOrder = scanner.next();
+                    switch (finalOrder){
+                        case "1" ->{
+                            System.out.println("Введіть назву кави яку хочете видалити");
+                            Scanner scannerOrder = new Scanner(System.in);
+                            String deleteOrder = scannerOrder.next();
+                            int index = 0;
+                            for (Caffe c : order.getTotalList()){
+                                if (deleteOrder == c.getName()){
+                                    order.getTotalList().remove(index);
+                                    index ++;
+                                }
+                            }
+
+                        }
+                    }
                 }
             }
 
